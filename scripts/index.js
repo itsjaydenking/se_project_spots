edfbdb80d650cb71223868d3a453f56cf0badd5c
Jayden King <itsjaydenking@gmail.com>
2025-06-26 16:44:09 -0700

    Refactor modal form handling: add placeholders for profile and card form elements

--- scripts/index.js
+++ scripts/index.js
@@ -21,3 +21,54 @@ newPostButton.addEventListener("click", () => {
 newPostModal.addEventListener("click", () => {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+/*
+// Select the necessary form elements. You should select
+// these from inside the modal, not the document.
+const profileFormElement = // Use querySelector()
+const nameInput = // Use querySelector()
+const jobInput = // Use querySelector()
+
+// If you haven't done so already, select
+// the profile elements from the document.
+const profileNameElement = // Use querySelector()
+const profileJobElement = // Use querySelector()
+
+// Create the form submission handler.
+function handleProfileFormSubmit(evt) {
+  // Prevent default browser behavior.
+  evt.preventDefault();
+
+  // Get the values of each form field from the value
+  // property of the corresponding input element.
+
+  // Insert these new values into the textContent
+  // property of the corresponding profile elements.
+
+  // Close the modal.
+}
+
+// Set the submit listener.
+profileFormElement.addEventListener('submit', handleProfileFormSubmit);
+*/
+
+/*
+// Select the necessary form elements. You should select
+// these from inside the modal, not the document.
+const addCardFormElement = // Use querySelector()
+const nameInput = // Use querySelector()
+const linkInput = // Use querySelector()
+
+// Create the form submission handler.
+function handleAddCardSubmit(evt) {
+  // Prevent default browser behavior.
+  evt.preventDefault();
+
+  // Log both input values to the console.
+
+  // Close the modal.
+}
+
+// Create the submit listener.
+addCardFormElement.addEventListener('submit', handleAddCardSubmit);
+*/
