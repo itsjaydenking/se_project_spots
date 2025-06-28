b3a617c88c5d4fb645e65dd971b9b735b45ac094
Jayden King <itsjaydenking@gmail.com>
2025-06-28 10:19:41 -0700

    Add initial cards data and log card names to console

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,29 @@
-const cardsContainer = document.querySelector(".cards");
+initialCards = [
+  {
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+  {
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  },
+  {
+    name: "An outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+  {
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+  {
+    name: "Mountain house",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+];
 
 // Profile Modal Functionality
 const editProfileButton = document.querySelector(".profile__edit-button");
@@ -81,3 +106,5 @@ function handleAddCardSubmit(evt) {
 
 // Create the submit listener.
 addCardFormElement.addEventListener("submit", handleAddCardSubmit);
+
+initialCards.forEach((card) => console.log(card.name));
